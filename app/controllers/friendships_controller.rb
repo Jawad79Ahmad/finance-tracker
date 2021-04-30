bc47e6a6314a83d9733b61acdd5575adab5164c6
Jawad79Ahmad <jawad79ahmad@gmail.com>
2021-04-30 16:37:06 +0500

    Added search,add and remove functionality of friend

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,21 @@
+class FriendshipsController < ApplicationController
+
+	def create
+		friend = User.find(params[:friend])
+		current_user.friendships.build(friend_id: friend.id)
+		if current_user.save
+			flash[:notice] = "Following user"
+		else
+			flash[:alert] = "Something wronge"
+		end
+		redirect_to my_friends_path
+	end
+
+	def destroy
+		friendship = current_user.friendships.where(friend_id: params[:id]).first
+		friendship.destroy
+		flash[:notice] = "Unfollowed"
+		redirect_to my_friends_path
+	end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,25 @@ class UsersController < ApplicationController
     @tracked_stock = current_user.stocks
   end
   def my_friends
-    @my_friend = current_user.friends
+    @friends = current_user.friends
+  end
+  def search
+    if params[:friend].present?
+      @friends = current_user.friends
+      @friend = User.search(params[:friend])
+      @friend = current_user.except_current_user(@friend)
+      if @friend
+        render 'users/my_friends'
+      else
+        flash.now[:alert] = "Enter a valid username or email."
+        render my_friends_path
+      end
+    else
+      flash.now[:alert] = "Enter a username or email to search friend"
+      render my_friends_path
+    end
+  end
+  def show
+
   end
 end
