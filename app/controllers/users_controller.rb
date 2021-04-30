d03575647062c3472604d3bcc5479afdde5cd211
Jawad79Ahmad <jawad79ahmad@gmail.com>
2021-04-30 10:45:03 +0500

    Friend list view and navbar update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def my_protfolio
     @tracked_stock = current_user.stocks
   end
+  def my_friends
+    @my_friend = current_user.friends
+  end
 end
